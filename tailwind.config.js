a8f890ff048db2ee9831cd5568d1a9bbceb0e06c
Noah <pro@noahcode.dev>
2024-08-03 02:11:49 +0200

    fix: some fix and tailwind
    add color

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media", // Enables dark mode based on the user's system preference
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
