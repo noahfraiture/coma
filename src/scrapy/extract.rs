22511e443db210371bff55a5e1736866019e8224
Noah <pro@noahcode.dev>
2024-08-14 00:57:52 +0200

    refactor: extract elements

--- src/scrapy/extract.rs
+++ src/scrapy/extract.rs
@@ -4,12 +4,11 @@ use std::{
 };
 
 use markup5ever::local_name;
-use scraper::Html;
+use scraper::{node::Element, Html};
 use url::Url;
 
 use crate::topology;
 
-// TODO: here hashset but other vector, should harmonized that
 pub fn extract_links(url: &Url, page: &Html) -> HashSet<Url> {
     HashSet::from_iter(page.tree.values().filter_map(|v| match v {
         scraper::Node::Element(element) => {
@@ -54,45 +53,43 @@ pub fn extract_texts(node: &Arc<Mutex<topology::Node>>, page: &Html) {
         .collect();
 }
 
-// TODO refactor to reduce duplicate code
-pub fn extract_images(node: &Arc<Mutex<topology::Node>>, page: &Html) {
-    node.lock().unwrap().images = page
-        .tree
+pub fn extract_element<T, F>(page: &Html, filter: F) -> Vec<T>
+where
+    F: Fn(Element) -> Option<T>,
+{
+    page.tree
         .values()
         .filter_map(|v| match v {
             scraper::Node::Element(element) => {
                 let element = element.to_owned();
-                if !matches!(element.name.local, local_name!("img")) {
-                    return None;
-                }
-                for (key, value) in &element.attrs {
-                    if matches!(key.local, local_name!("src")) {
-                        // TODO: add errors
-                        // If the url is absolute, the value will replace the base url
-                        return Url::join(&node.lock().unwrap().url, value).ok();
-                    }
-                }
-                None
+                filter(element)
             }
             _ => None,
         })
-        .collect();
+        .collect()
 }
 
-pub fn extract_input(node: &Arc<Mutex<topology::Node>>, page: &Html) {
-    node.lock().unwrap().inputs = page
-        .tree
-        .values()
-        .filter_map(|v| match v {
-            scraper::Node::Element(element) => {
-                let element = element.to_owned();
-                if matches!(element.name.local, local_name!("input")) {
-                    Some(format!("{:?}", element))
-                } else {
-                    None
+pub fn extract_images(node: &Arc<Mutex<topology::Node>>, page: &Html) {
+    node.lock().unwrap().images = extract_element(page, |element: Element| {
+        if matches!(element.name.local, local_name!("img")) {
+            for (key, value) in &element.attrs {
+                if matches!(key.local, local_name!("src")) {
+                    // If the url is absolute, the value will replace the base url
+                    return Url::join(&node.lock().unwrap().url, value).ok();
                 }
             }
-            _ => None,
-        })
-        .collect();
+        }
+        None
+    });
+}
+
+// TODO : better rendering
+pub fn extract_input(node: &Arc<Mutex<topology::Node>>, page: &Html) {
+    node.lock().unwrap().inputs = extract_element(page, |element: Element| {
+        if matches!(element.name.local, local_name!("input")) {
+            Some(format!("{:?}", element))
+        } else {
+            None
+        }
+    });
 }
